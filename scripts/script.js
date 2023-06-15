b5655dae932eef55ad930d4282c5f0f1ad481c3d
Steven Beckham <stevencbeckham@gmail.com>
2023-06-15 10:23:31 -0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+  /*
+  var currentDate = new Date();  // Get the current date
+  var day = currentDate.getDate();  // Get the day of the month (1-31)
+  var month = currentDate.getMonth() + 1;  // Get the month (0-11, hence +1)
+
+  // Redirect based on the current date
+  if (month === 6 && day === 13) {
+    window.location.href = "/html/june/jun-13.html";
+  } else if (month === 6 && day === 14) {
+    window.location.href = "/html/june/jun-14.html";
+  } else {
+    window.location.href = "/html/june/jun-15.html";  // Default page if no specific date matches
+  }
+  */
